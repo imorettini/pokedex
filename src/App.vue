24531581a3428cbeafaa945592c43c3f623d78f6
Baco Morettini <imorettini@gmail.com>
2022-04-14 19:59:02 -0300

    inserindo dialog info dos pokes

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,58 @@
 <template>
   <v-app>
     <v-container>
+      <v-container>
+        <v-text-field
+          v-model="search"
+          label="Pesquisar"
+          placeholder="Pesquisar pokémon"
+          solo
+        ></v-text-field>
+        <v-row>
+          <v-col
+            cols="2"
+            v-for="pokemon in filtered_pokemons"
+            :key="pokemon.name"
+          >
+            <v-card @click="show_pokemon(get_id(pokemon))">
+              <v-container>
+                <v-row class="mx-0 d-flex justify-center">
+                  <img
+                    :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
+                      pokemon
+                    )}.png`"
+                    :alt="pokemon.name"
+                    width="80%"
+                  />
+                </v-row>
+                <h2 class="text-center">{{ get_name(pokemon) }}</h2>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+    <v-dialog v-model="show_dialog" width="800">
+      <v-card v-if="selected_pokemon">
         <v-container>
-          <v-text-field
-            v-model="search"
-            label="Pesquisar"
-            placeholder="Pesquisar pokémon"
-            solo
-          ></v-text-field>
-          <v-row>
-            <v-col
-              cols="2"
-              v-for="pokemon in filtered_pokemons"
-              :key="pokemon.name"
-            >
-              <v-card>
-                <v-container>
-                  <v-row class="mx-0 d-flex justify-center">
-                    <img
-                      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
-                        pokemon
-                      )}.png`"
-                      :alt="pokemon.name"
-                      width="80%"
-                    />
-                  </v-row>
-                  <h2 class="text-center">{{ get_name(pokemon) }}</h2>
-                </v-container>
-              </v-card>
+          <v-row class="d-flex align-center">
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selected_pokemon.id}.png`"
+                :alt="selected_pokemon.name"
+                width="80%"
+              />
+            </v-col>
+            <v-col cols="8">
+              <h1>{{ get_name(selected_pokemon) }}</h1>
+              <v-chip>Altura: {{ selected_pokemon.height * 2.54 }}cm</v-chip>
+              <v-chip class="ml-2">Peso: {{ (selected_pokemon.weight * 0.45359237).toFixed(0) }}kg</v-chip>
             </v-col>
           </v-row>
+          {{ selected_pokemon }}
         </v-container>
-    </v-container>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
@@ -46,6 +67,8 @@ export default {
     return {
       pokemons: [],
       search: "",
+      show_dialog: false,
+      selected_pokemon: null,
     };
   },
 
@@ -65,6 +88,12 @@ export default {
     get_name(pokemon) {
       return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     },
+    show_pokemon(id) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+        this.selected_pokemon = response.data;
+        this.show_dialog = !this.show_dialog;
+      });
+    },
   },
   computed: {
     filtered_pokemons() {
@@ -78,12 +107,8 @@ export default {
 
 <style>
 #app {
-  background: linear-gradient(
-      to bottom right,
-      rgba(10, 10, 10, 1),
-      rgba(12, 39, 63, 1)
-    )
-    no-repeat center center fixed !important;
+background: rgb(183,171,197);
+background: linear-gradient(90deg, rgba(183,171,197,1) 0%, rgba(166,168,216,1) 21%, rgba(164,231,162,1) 48%, rgba(231,233,158,1) 66%, rgba(233,194,153,1) 87%, rgba(233,148,148,1) 100%);
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
