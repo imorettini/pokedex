0a43c87cd05b240db8b8c47f9b872495f14fc1fc
Baco Morettini <imorettini@gmail.com>
2022-04-14 11:49:24 -0300

    iniciando pokedex

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-card>
+        <v-container>
+          <v-row>
+            <v-col cols="3" v-for="pokemon in pokemons" 
+            :key="pokemon.name">
+            <h2>{{pokemon.name}}</h2>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+
+  components: {},
+
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+
+  mounted() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => {
+        this.pokemons = response.data.results;
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  background: linear-gradient(
+      to bottom right,
+      rgba(10, 10, 10, 1),
+      rgba(12, 39, 63, 1)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+}
+</style>
